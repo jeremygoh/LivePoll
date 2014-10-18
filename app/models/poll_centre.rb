44f66390f29effb3f3e02b41c81e247491c7d257
Jeremy Goh <jg485host@gmail.com>
2014-10-17 21:06:03 -0700

    added url formatting for title

--- app/models/poll_centre.rb
+++ app/models/poll_centre.rb
@@ -2,4 +2,9 @@ class PollCentre < ActiveRecord::Base
   belongs_to :user
   has_many :questions
   validates :title, uniqueness: true, presence: true
+  before_save :format_title
+
+  def format_title
+    self.title = title.strip.gsub(/\s+/, '-').downcase
+  end
 end
